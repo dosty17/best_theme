59e2dc83b10cc83d08ba66578c5cf8d71ae90d13
dosty17 <doste.pshtiwan@gmail.com>
2026-06-19 19:31:38 +0300

    version-3

--- best_theme/lib/src/best_theme_storage.dart
+++ best_theme/lib/src/best_theme_storage.dart
@@ -1,6 +1,30 @@
 import 'package:flutter/material.dart';
 
+/// An abstract class that defines the interface for theme storage.
 abstract class BestThemeStorage {
+  /// Saves the current theme mode to storage.
+  /// You can implement this method to save the theme mode to shared preferences, a database, or any other storage mechanism.
+  ///
+  /// example:
+  /// ```dart
+  /// SharedPreferences prefs = await SharedPreferences.getInstance();
+  /// await prefs.setString('themeMode', mode.toString());
+  /// ```
+  ///
   Future<void> saveThemeMode(ThemeMode mode);
+
+  /// Loads the saved theme mode from storage.
+  /// You can implement this method to load the theme mode from shared preferences, a database, or any other storage mechanism.
+  ///
+  /// example:
+  /// ```dart
+  /// SharedPreferences prefs = await SharedPreferences.getInstance();
+  /// String? themeModeString = prefs.getString('themeMode');
+  /// if (themeModeString != null) {
+  ///   return ThemeMode.values.firstWhere((mode) => mode.toString() == theme
+  /// ModeString);
+  /// }
+  /// return null;
+  /// ```
   Future<ThemeMode?> loadThemeMode();
 }
